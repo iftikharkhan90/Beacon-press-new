2e50d078bda24c83cc5ba12c4c952ab55b3eaf07
asif445123 <masifrana445@gmail.com>
2025-10-10 16:18:46 +0500

    fixed

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
                 {isDropdownOpen && (
                   <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg  z-10">
                     <Link
-                      to={`/edituser/${user?.id}`}
+                      to={`/patch/${user?.id}`}
                       className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 text-sm"
                       onClick={() => setIsDropdownOpen(false)}
                     >
